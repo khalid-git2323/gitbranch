a5aa1d8105283bb427c991c8b79333b06d076c62
Khalid Jamadar <khalidjamadar100@gmail.com>
2025-09-08 16:17:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,6 @@
             padding: 40px;
             background: url('https://images.unsplash.com/photo-1519389950473-47ba0277781c') no-repeat center center fixed;
             background-size: cover;
-            colour ="red"
-            colour ="green"
             colour ="blue-dark"
         }
 
